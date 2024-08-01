c20162a86bb41385e01bd9723acb3e8785165079
eric <ericjin@shein.com>
2024-08-01 19:58:17 +0800

    提交B

---
+++ demo/src/main/java/com/example/demo/test/B.java
@@ -0,0 +1,15 @@
+package com.example.demo.test;
+
+/**
+ * <p>
+ * TODO
+ * </p>
+ *
+ * @author Eric Jin
+ * @since 2024/8/1 19:55
+ */
+public class B {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
